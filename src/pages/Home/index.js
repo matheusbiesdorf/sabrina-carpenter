eed1061266f3ce0436cf1b8dd8a517e8dd1632e9
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-02-15 03:14:30 -0300

    Social media icons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,10 @@
 import styles from "./Home.module.css";
 import imagemHome1 from './sns-deluxe.jpg'
+import facebookLogo from './facebook-logo.png'
+import instagramLogo from './instagram-logo.png'
+import tiktokLogo from './tiktok-logo.png'
+import XLogo from './X-logo.png'
+import youtubeLogo from './youtube-logo.png'
 
 export default function Home() {
    return (
@@ -10,7 +15,15 @@ export default function Home() {
                <img src={imagemHome1} />
                <h2>NEW RELEASE: SHORT N' SWEET (DELUXE) OUT NOW</h2>
             </div>
-            
+
+            <div className={styles.socialMedia}>
+               <img src={facebookLogo} />
+               <img src={instagramLogo} />
+               <img src={XLogo} />
+               <img src={youtubeLogo} />
+               <img src={tiktokLogo} />
+            </div>
+
          </main>
       </>
    );
