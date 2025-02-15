15b3921d011220c660cec3de975d020b3c8a85b0
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-02-15 00:50:44 -0300

    Navbar completed

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,37 @@
-import NavbarLink from "components/NavbarLink";
-import styles from './Navbar.module.css'
+// Navbar.jsx
+import React, { useState } from 'react';
+import NavbarLink from 'components/NavbarLink'; 
+import { FaBars, FaTimes } from 'react-icons/fa'; 
+import styles from './Navbar.module.css'; 
 
-export default function Navbar(){
-    return(
-        <nav className={styles.navbar}>
-            <NavbarLink to="/" >Home</NavbarLink>
-            <NavbarLink to="/music" >Music</NavbarLink>
-            <NavbarLink to="/shop" >Shop</NavbarLink>
-            <NavbarLink to="/tour" >Tour</NavbarLink>
-            <NavbarLink to="/videos" >Videos</NavbarLink>
-            <NavbarLink to="/signup" >Sign Up</NavbarLink>
-        </nav>
-    )
+export default function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
+  const closeMenu = () => {
+    setIsMobile(false);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+
+        <ul className={isMobile ? styles.navLinksMobile : styles.navLinks}>
+          <li onClick={closeMenu}><NavbarLink to="/">Home</NavbarLink></li>
+          <li onClick={closeMenu}><NavbarLink to="/music">Music</NavbarLink></li>
+          <li onClick={closeMenu}><NavbarLink to="/shop">Shop</NavbarLink></li>
+          <li onClick={closeMenu}><NavbarLink to="/tour">Tour</NavbarLink></li>
+          <li onClick={closeMenu}><NavbarLink to="/videos">Videos</NavbarLink></li>
+          <li onClick={closeMenu}><NavbarLink to="/signup">Sign Up</NavbarLink></li>
+        </ul>
+
+        <div className={styles.menuIcon} onClick={toggleMenu}>
+          {isMobile ? <FaTimes /> : <FaBars />}
+        </div>
+      </div>
+    </nav>
+  );
 }
