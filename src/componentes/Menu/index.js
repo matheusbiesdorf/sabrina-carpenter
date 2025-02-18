23317410fe0bf2ae9ded41c522f491437cce1f54
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-02-17 22:10:17 -0300

    Barra de navegação

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,5 +1,29 @@
-import styles from './Menu.module.css'
+import { useState } from "react";
+import styles from "./Menu.module.css";
+import { NavLink } from "react-router-dom";
+import MenuLink from "componentes/MenuLink";
 
-export default function Menu() {
-   return <></>;
-}
+const Menu = () => {
+   const [isOpen, setIsOpen] = useState(false);
+
+   return (
+      <nav className={styles.navbar}>
+         <button
+            className={styles.hamburger}
+            onClick={() => setIsOpen(!isOpen)}
+         >
+            ☰
+         </button>
+         <ul className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+            
+            <li> <MenuLink to="/">Home</MenuLink> </li>
+            <li> <MenuLink to="/music">Music</MenuLink> </li>
+            <li> <MenuLink to="/videos">Videos</MenuLink> </li>
+            <li> <MenuLink to="/shop">Shop</MenuLink> </li>
+            <li> <MenuLink to="/signup">Sign Up</MenuLink> </li>
+         </ul>
+      </nav>
+   );
+};
+
+export default Menu;
--- src/componentes/MenuLink/index.js
+++ src/componentes/MenuLink/index.js
@@ -1,5 +1,10 @@
-import styles from './MenuLink.module.css'
+import { NavLink } from "react-router-dom";
+import styles from "./MenuLink.module.css";
 
-export default function MenuLink() {
-   return <></>;
+export default function MenuLink({ to, children }) {
+   return (
+   <NavLink to={to} className={styles.MenuLink}>
+      {children}
+   </NavLink>
+   )
 }
