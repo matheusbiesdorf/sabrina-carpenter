7d519cba13f1312822a845e6363caea4ad141a92
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-02-17 23:46:59 -0300

    Página Home

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,16 +1,28 @@
-import styles from './Home.module.css'
-import backgroundDesktop from './bg-desktop.png'
-import backgroundMobile from './bg-mobile.png'
+import styles from "./Home.module.css";
+import backgroundDesktop from "./bg-desktop.png";
+import backgroundMobile from "./bg-mobile.png";
+import snsDeluxe from "./sns-deluxe.jpg";
 
-export default function Home(){
-    return(
-        <main className={styles.mainHome}>
-            <section className={styles.homeNews}>
+export default function Home() {
+   return (
+      <main className={styles.mainHome}>
 
-            </section>
-            <section className={styles.redesSociais}>
+         
+            <div className={styles.homeNews}>
+               <img src={snsDeluxe} />
+               <h2>NEW RELEASE: SHORT N' SWEET (DELUXE) OUT NOW</h2>
+               <p>
+                  featuring the hits singles Espresso, Please, Please, Please
+                  and Taste.
+               </p>
+            </div>
+         
 
-            </section>
-        </main>
-    )
+         <section className={styles.redesSociais}>
+            <div>
+
+            </div>
+         </section>
+      </main>
+   );
 }
