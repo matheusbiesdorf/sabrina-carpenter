14fd3fa0d3ae0cd348afa35a5ee3b48374181b96
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-02-19 02:39:50 -0300

    Ajuste na lógica do menu mobile

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -5,6 +5,8 @@ import MenuLink from "componentes/MenuLink";
 const Menu = () => {
    const [isOpen, setIsOpen] = useState(false);
 
+   const closeMenu = () => setIsOpen(false); // Fecha o menu ao clicar em um link
+
    return (
       <nav className={styles.navbar}>
          <button
@@ -14,12 +16,11 @@ const Menu = () => {
             ☰
          </button>
          <ul className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
-            
-            <li> <MenuLink to="/">Home</MenuLink> </li>
-            <li> <MenuLink to="/music">Music</MenuLink> </li>
-            <li> <MenuLink to="/videos">Videos</MenuLink> </li>
-            <li> <MenuLink to="/shop">Shop</MenuLink> </li>
-            <li> <MenuLink to="/signup">Sign Up</MenuLink> </li>
+            <li><MenuLink to="/" onClick={closeMenu}>Home</MenuLink></li>
+            <li><MenuLink to="/music" onClick={closeMenu}>Music</MenuLink></li>
+            <li><MenuLink to="/videos" onClick={closeMenu}>Videos</MenuLink></li>
+            <li><MenuLink to="/shop" onClick={closeMenu}>Shop</MenuLink></li>
+            <li><MenuLink to="/signup" onClick={closeMenu}>Sign Up</MenuLink></li>
          </ul>
       </nav>
    );
--- src/componentes/MenuLink/index.js
+++ src/componentes/MenuLink/index.js
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom";
 import styles from "./MenuLink.module.css";
 
-export default function MenuLink({ to, children }) {
+export default function MenuLink({ to, children, onClick }) {
    return (
-   <NavLink to={to} className={styles.MenuLink}>
-      {children}
-   </NavLink>
-   )
+      <NavLink to={to} className={styles.MenuLink} onClick={onClick}>
+         {children}
+      </NavLink>
+   );
 }
