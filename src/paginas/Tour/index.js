43c802462fd1dae38987184f8e51166e1aa15a24
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-02-21 16:20:28 -0300

    json com informações dos shows

--- src/paginas/Tour/index.js
+++ src/paginas/Tour/index.js
@@ -2,6 +2,31 @@ import styles from './Tour.module.css'
 
 export default function Tour(){
     return(
-        <h2>Tour</h2>
+        <main className={styles.mainTour}>
+            <h2>SHORT N' SWEET TOUR</h2>
+            <img src='' />
+
+
+            <ul>
+
+
+
+                <li>
+                    <div>
+                        <span>Data</span>
+                        <div>
+                            <span>Local</span>
+                            <span>Cidade e país</span>
+                        </div>
+                        <button>Tickets</button>
+                    </div>
+                </li>
+
+
+
+            </ul>
+
+
+        </main>
     )
 }
