bf497ed33eb79ad21f3a3ea7c2f461f2c8483e34
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-02-21 03:29:41 -0300

    json dos produtos

--- src/paginas/Shop/index.js
+++ src/paginas/Shop/index.js
@@ -3,7 +3,8 @@ import styles from './Shop.module.css'
 export default function Shop(){
     return (
         <main className={styles.mainShop}>
-            <h1>Loja</h1>
+            BANNER COM O VÍDEO
+            FILTROS --- PRODUTOS (CardProduto)
         </main>
     )
 }
